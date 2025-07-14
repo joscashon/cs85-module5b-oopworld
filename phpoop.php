6d9b68f996f3cfa6530b7cc20753d4f9224f1e0d
Jos Cashon <joscashon@gmail.com>
2025-07-13 22:40:20 -0700

    Added getters and setters

--- phpoop.php
+++ phpoop.php
@@ -11,5 +11,37 @@ class PriceCandle {
         $this->low = $low;
         $this->close = $close;
     }
+
+    public function getOpen() {
+        return $this->open;
+    }
+
+    public function getHigh() {
+        return $this->high;
+    }
+
+    public function getLow() {
+        return $this->low;
+    }
+
+    public function getClose() {
+        return $this->close;
+    }
+
+    public function setOpen($open) {
+        $this->open = $open;
+    }
+
+    public function setHigh($high) {
+        $this->high = $high;
+    }
+
+    public function setLow($low) {
+        $this->low = $low;
+    }
+
+    public function setClose($close) {
+        $this->close = $close;
+    }
 }
 ?>
